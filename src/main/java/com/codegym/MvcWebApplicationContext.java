182941bc9c1cac7762da29d54b095bddc93b53f1
Minh Lee <minhlee.fre.mta@gmail.com>
2018-08-31 17:42:14 +0700

    declare root config class and servlet config class to return

--- src/main/java/com/codegym/MvcWebApplicationContext.java
+++ src/main/java/com/codegym/MvcWebApplicationContext.java
@@ -5,12 +5,12 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class MvcWebApplicationContext extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{WebConfig.class};
+        return new Class[]{WebSecurityConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebConfig.class};
     }
 
     @Override
